8a1253648d493892d6dfd184c6ea75d9f0b87159
Cyril <sahayacyril2015@gmail.com>
2021-02-12 03:30:04 +0530

    Add styles

--- app.js
+++ app.js
@@ -5,22 +5,23 @@ const bodyParser = require("body-parser");
 
 const app =  express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(res, res) {
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
         kindOfDay: day,
