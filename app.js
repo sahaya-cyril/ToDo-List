a8052136836af6ee4a5cbed7ae2a7e17b2283b64
Cyril <sahayacyril2015@gmail.com>
2021-02-12 01:59:03 +0530

    Modify date

--- app.js
+++ app.js
@@ -9,32 +9,18 @@ app.set("view engine", "ejs");
 
 app.get("/", function(res, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    if (currentDay == 0) {
-        day = "Sunday";
-    }
-    else if (currentDay == 1){
-        day = "Monday";
-    }
-    else if (currentDay == 2) {
-        day = "Tuesday";
-    }
-    else if (currentDay == 3) {
-        day = "Wednesday";
-    }
-    else if (currentDay == 4) {
-        day = "Thursday";
-    }
-    else if (currentDay == 5) {
-        day = "Friday";
-    }
-    else {
-        day == "Saturday";
-    }
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    res.render("list", {kindOfDay: day});
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {
+        kindOfDay: day
+    });
 });
 
 app.listen("3000", function(req, res) {
