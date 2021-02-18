efa2b993595c680b29b264ed3c82fc4d9f047abd
Cyril <sahayacyril2015@gmail.com>
2021-02-19 02:26:23 +0530

    Connect to MongoDB

--- app.js
+++ app.js
@@ -2,17 +2,45 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app =  express();
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+const itemsSchema = {
+    name: String
+}
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfullt saved default items to database.");
+    }
+});
 
 app.get("/", function(res, res) {
     const day = date.getDate();
